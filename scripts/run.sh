63c0157dac6a9fa0234b60bbf87cadb4c9bfaa4a
Franco Liza <franco.s.liza@gmail.com>
2021-10-26 18:58:03 -0300

    modificacion de run.sh - toma la mediana del tiempo

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-FORMAT="iteracion,threads,tiempo,output"
+FORMAT="threads_lectura,thread_maximo,tiempo_mediana,output"
 file1='data/corpus'
 #cat $file1 funciona!
 
 cd ..
 make clean
 make
-rm -rf output
+rm -rf output temp.txt out.txt
 mkdir output
 
 echo $FORMAT > output/salida.csv
 
-for i in {1..26}
+for i in {1..3}
 do  
-    ./build/ContarPalabras $i $i $file1 &> temp.txt
-    DATA=$(<temp.txt)
-    echo $DATA &> temp.txt
-    awk -v iteracion=$i -v threads=$i '{print iteracion","threads","$1","$2 $3}' temp.txt &>> output/salida.csv
+    for j in {1..5} 
+    do        
+        ./build/ContarPalabras 1 $i $file1 &> out.txt
+        DATA=$(<out.txt)
+        echo $DATA &>> temp.txt
+    done 
+    maximo=$(sed -n 1p temp.txt | awk '{print $2"="$3}')
+    awk '{print $1}' temp.txt | sort &> sorted.txt
+    sed -n 3p sorted.txt > median.txt
+    awk -v threads_lectura=$i -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/salida.csv
+    rm temp.txt out.txt sorted.txt
 done
 
-rm -rf temp.txt
