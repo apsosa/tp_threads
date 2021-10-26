0056e8cd056b6c043f3947fcd512239646b05f41
Franco Liza <franco.s.liza@gmail.com>
2021-10-26 19:57:05 -0300

    añadiendo pequeños cambios en run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cantidad_de_letras=26
 FORMAT="threads_lectura,thread_maximo,tiempo_mediana,output"
 file1='data/corpus'
 #cat $file1 funciona!
@@ -12,7 +13,7 @@ mkdir output
 
 echo $FORMAT > output/salida.csv
 
-for i in {1..3}
+for i in {1..26}
 do  
     for j in {1..5} 
     do        
@@ -20,10 +21,11 @@ do
         DATA=$(<out.txt)
         echo $DATA &>> temp.txt
     done 
+    
     maximo=$(sed -n 1p temp.txt | awk '{print $2"="$3}')
     awk '{print $1}' temp.txt | sort &> sorted.txt
     sed -n 3p sorted.txt > median.txt
-    awk -v threads_lectura=$i -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/salida.csv
+    awk -v threads_lectura=1 -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/salida.csv
     rm temp.txt out.txt sorted.txt
 done
 
