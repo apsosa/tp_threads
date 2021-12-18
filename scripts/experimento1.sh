06ba0810f5bcd82766d9de4c7d2b5c55a11ec4f7
Franco Liza <franco.s.liza@gmail.com4>
2021-12-18 17:47:35 -0300

    modificando experimento1.sh - sólo tiene en cuenta los repetidos

--- scripts/experimento1.sh
+++ scripts/experimento1.sh
@@ -3,7 +3,6 @@
 cantidad_de_letras=26
 FORMAT="threads_lectura,thread_maximo,tiempo_mediana,output"
 directoryRepetidos="data/random-con-repetidos"
-directoryNoRepetidos="data/random-sin-repetidos"
 
 sinConcurrencia=0
 conConcurrencia=1
@@ -21,10 +20,37 @@ echo "##########################################################################
 echo "LA SALIDA DE ESTE EJECUTABLE SE ENCUENTRA EN LA CARPETA OUTPUT EN LA RAIZ DEL PROYECTO"
 echo "######################################################################################"
 
-for file in data/random-sin-repetidos/*
+echo "######### Calculamos el máximo de manera SECUENCIAL con los datasets creados #########"
+
+for file in data/random-con-repetidos/*
 do
     files=$(basename "$file")
-    echo $FORMAT > output/salida-$files.csv
+    echo $FORMAT > output/experimento1/sin_concurrencia/salida-$files.csv
+    echo $files
+    for i in {1..26}
+    do  
+        for j in {1..5} 
+        do        
+            ./build/ContarPalabras 1 $i $sinConcurrencia $file  &> out.txt
+            DATA=$(<out.txt)
+            echo $DATA &>> temp.txt
+        done 
+        
+        maximo=$(sed -n 1p temp.txt | awk '{print $2"="$3}')
+        awk '{print $1}' temp.txt | sort &> sorted.txt
+        sed -n 3p sorted.txt > median.txt
+        awk -v threads_lectura=1 -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/experimento1/sin_concurrencia/salida-$files.csv
+        rm temp.txt out.txt sorted.txt median.txt
+    done
+done
+
+echo "######### Calculamos el máximo de manera PARALELA con los datasets creados #########"
+
+for file in data/random-con-repetidos/*
+do
+    files=$(basename "$file")
+    echo $FORMAT > output/experimento1/con_concurrencia/salida-$files.csv
+
     echo $files
     for i in {1..26}
     do  
@@ -38,31 +64,8 @@ do
         maximo=$(sed -n 1p temp.txt | awk '{print $2"="$3}')
         awk '{print $1}' temp.txt | sort &> sorted.txt
         sed -n 3p sorted.txt > median.txt
-        awk -v threads_lectura=1 -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/salida-$files.csv
+        awk -v threads_lectura=1 -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/experimento1/con_concurrencia/salida-$files.csv
         rm temp.txt out.txt sorted.txt median.txt
     done
 done
 
-# Para los repetidos
-
-for file in data/random-con-repetidos/*
-do
-    files=$(basename "$file")
-    echo $FORMAT > output/salida-$files.csv
-    echo $files
-    for i in {1..26}
-    do  
-        for j in {1..5} 
-        do        
-            ./build/ContarPalabras 1 $i $conConcurrencia $file  &> out.txt
-            DATA=$(<out.txt)
-            echo $DATA &>> temp.txt
-        done 
-        
-        maximo=$(sed -n 1p temp.txt | awk '{print $2"="$3}')
-        awk '{print $1}' temp.txt | sort &> sorted.txt
-        sed -n 3p sorted.txt > median.txt
-        awk -v threads_lectura=1 -v threads_maximo=$i -v salida=$maximo '{print threads_lectura","threads_maximo","$1","salida}' median.txt &>> output/salida-$files.csv
-        rm temp.txt out.txt sorted.txt median.txt
-    done
-done
